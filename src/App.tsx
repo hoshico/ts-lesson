cae5e3a96a9c0098422dba5a9b279475bb2bc5a5
hoshico <melancholinista.yasu@gmail.com>
2021-12-05 11:21:30 +0900

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createLogicalNot } from 'typescript';
+
+let username: string  = "hello";
+let dummyNum: number = 2;
+let bool: boolean = true;
+
+let array = [true, false, true];
+let array2 = [0, 1, "hello"];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+type PROFILE = {
+  age: number;
+  city: string;
+}
+
+type LOGIN = {
+  username: string;
+  password: string;
+}
+
+//intersection types
+type USER = PROFILE & LOGIN;
+
+const  userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: "xxx",
+  password: "yyy",
+}
+
+let nameObj: NAME = { first: "Yamada" , last: "Taro"};
+
+const fun01 = (x: number, y: number): number => {
+ return x + y;
+}
 
 function App() {
   return (
